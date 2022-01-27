a14eca5507c843c05ff411cd7278bf6a177fc43c
b1u3too <almadsen792@live.ca>
2022-01-27 05:17:34 +0000

    Initialized assertEqual function w/ emoji messages

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅🥳✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑😱🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(3, 25);
+assertEqual(3,'3'); //this test displays funkily, will explore how to make that more user friendly in future
