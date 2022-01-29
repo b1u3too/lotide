8d496cd1eeae265578803a20975561da84cdbc08
b1u3too <almadsen792@live.ca>
2022-01-29 18:11:42 +0000

    Clarified expected results on test cases

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -20,9 +20,9 @@ const assertArraysEqual = function(firstArray, secondArray) {
   }
 };
 
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([],[]);
-assertArraysEqual([],[""]);
-assertArraysEqual([1,2,3],[3,2,1]);
-assertArraysEqual(['potato', 'tomato', 'asparagus'],['potato', 'tomato']);
-assertArraysEqual(['potato', 'tomato', 'asparagus'],['potato', 'tomato', 'asparagus']);
+assertArraysEqual([1,2,3],[1,2,3]); //true
+assertArraysEqual([],[]); //true
+assertArraysEqual(['potato', 'tomato', 'asparagus'],['potato', 'tomato', 'asparagus']); //true
+assertArraysEqual([],[""]); //false
+assertArraysEqual([1,2,3],[3,2,1]); //false
+assertArraysEqual(['potato', 'tomato', 'asparagus'],['potato', 'tomato']); //false
