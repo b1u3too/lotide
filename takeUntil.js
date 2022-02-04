eec110a5f234dc4c22c338f7e37606984ec7cc21
b1u3too <almadsen792@live.ca>
2022-02-04 23:45:58 +0000

    Refactored for efficiency

--- takeUntil.js
+++ takeUntil.js
@@ -21,16 +21,13 @@ const assertArraysEqual = function(firstArray, secondArray) {
 };
 
 const takeUntil = function(array, callback) {
-  const results = [];
-
-  for (const item of array) {
-    if (callback(item)) {
-      return results;
-    } 
-    results.push(item);
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return array.slice(0, i);
+    }
   }
-  return results;
-}
+  return array;
+};
 
 assertArraysEqual(takeUntil([1,6,5,1,3,4], num => num % 5 === 0),[1,6]);
 assertArraysEqual(takeUntil(['a','b','c','d','e','g','f', 'lighthouse'], str => str.includes('h')),['a','b','c','d','e','g','f']);
