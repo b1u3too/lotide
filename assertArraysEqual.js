8b34e450137fa397bc77eb5351b1a674a2740e12
b1u3too <almadsen792@live.ca>
2022-02-16 05:51:29 +0000

    import/export of required functions, transferred test cases to separate test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(firstArray, secondArray) {
   if (eqArrays(firstArray, secondArray)) {
@@ -20,9 +8,4 @@ const assertArraysEqual = function(firstArray, secondArray) {
   }
 };
 
-assertArraysEqual([1,2,3],[1,2,3]); //true
-assertArraysEqual([],[]); //true
-assertArraysEqual(['potato', 'tomato', 'asparagus'],['potato', 'tomato', 'asparagus']); //true
-assertArraysEqual([],[""]); //false
-assertArraysEqual([1,2,3],[3,2,1]); //false
-assertArraysEqual(['potato', 'tomato', 'asparagus'],['potato', 'tomato']); //false
+module.exports = assertArraysEqual;
