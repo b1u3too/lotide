5281a654bd9e807f140d46a6fdc21011b9a19666
b1u3too <almadsen792@live.ca>
2022-02-16 05:35:02 +0000

    removed test cases and removed require assertEqual -- added exports head function

--- head.js
+++ head.js
@@ -1,11 +1,5 @@
-const assertEqual = require('./assertEqual');
-
 const head = function(inputArray) {
   return inputArray[0];
 };
 
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Important Notice"]), "Important Notice");
-assertEqual(head([]), undefined);
+module.exports = head;
