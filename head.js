ca27c558a9556361227923f89aa770d440a2e96a
b1u3too <almadsen792@live.ca>
2022-02-16 05:27:42 +0000

    imported assertEquals function

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅🥳✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑😱🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(inputArray) {
   return inputArray[0];
