6368b30d5cd5bc5349b430176d754990dc5a4c71
b1u3too <almadsen792@live.ca>
2022-02-16 05:31:30 +0000

    transferred assertEqual test scripts into separate test file (assertEqual.test.js)

--- assertEqual.js
+++ assertEqual.js
@@ -7,10 +7,3 @@ const assertEqual = function(actual, expected) {
 };
 
 module.exports = assertEqual;
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(3, 25);
-assertEqual(3,'3'); //this test displays funkily, will explore how to make that more user friendly in future
---
+++ test/assertEqual.test.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(3, 25);
+assertEqual(3,'3'); //this test displays funkily, will explore how to make that more user friendly in future
