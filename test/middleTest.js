cd499a5fdcc72865d25c04580fa15c87c86b0eda
b1u3too <almadsen792@live.ca>
2022-02-17 02:46:14 +0000

    implemented mocha automated tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,22 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([]),[]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]),[]);
-assertArraysEqual(middle([1,2,3]),[2]);
-assertArraysEqual(middle([1,2,3,4,5]),[3]);
-assertArraysEqual(middle([1,2,3,4]),[2,3]);
-assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
+describe("#middle", () => {
+  it("should return [] when given []", () => {
+    assert.deepEqual(middle([]),[]);
+  });
+  it("should return [] when given a single element array", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("should return [] when given a two element array", () => {
+    assert.deepEqual(middle([1,2]),[]);
+  });
+  it("should return the [middle element] for arrays of odd length", () => {
+    assert.deepEqual(middle([1,2,3]),[2]);
+    assert.deepEqual(middle([1,2,3,4,5]),[3]);
+  });
+  it("should return the [middle, elements] for arrays of even length", () => {
+    assert.deepEqual(middle([1,2,3,4]),[2,3]);
+    assert.deepEqual(middle([1,2,3,4,5,6]),[3,4]);
+  });
+});
