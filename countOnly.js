2c4f45d5c70d22c4ed90f8a87ee384afa83adc28
b1u3too <almadsen792@live.ca>
2022-02-17 05:45:09 +0000

    Added clarifying comments

--- countOnly.js
+++ countOnly.js
@@ -2,7 +2,9 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
+    //is this item meant to be counted?
     if (itemsToCount[item]) {
+      //Check if a tally has started. Yes? Add 1. No? Start the tally at 1.
       if (results[item]) {
         results[item] += 1;
       } else {
