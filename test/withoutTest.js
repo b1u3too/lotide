11ad241d6fa8edd487d5686e89156abfd7c2beab
b1u3too <almadsen792@live.ca>
2022-02-19 04:17:04 +0000

    syntax correction for assertion scripts

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -5,13 +5,13 @@ describe("#without", () => {
   it("should not mutate the source array", () => {
     const testing = [1,2,3];
     without(testing, [2]);
-    assertArraysEqual(testing, [1,2,3]);  
+    assert.deepEqual(testing, [1,2,3]);  
   });
   it("should not return any instances of the indicated elements in the new array", () => {
-    assertArraysEqual(without([1,2,3],[2]), [1,3]);
-    assertArraysEqual(without([1,2,2,2,2,2,3],[2]), [1,3]);
-    assertArraysEqual(without(['1','2',3],[1,2]),['1','2',3]);
-    assertArraysEqual(without(['Potato', 'potato', 'tomato'],['potato']),['Potato', 'tomato']);
-    assertArraysEqual(without(['ready','freddy'],['let\'s', 'go']),['ready', 'freddy']);
+    assert.deepEqual(without([1,2,3],[2]), [1,3]);
+    assert.deepEqual(without([1,2,2,2,2,2,3],[2]), [1,3]);
+    assert.deepEqual(without(['1','2',3],[1,2]),['1','2',3]);
+    assert.deepEqual(without(['Potato', 'potato', 'tomato'],['potato']),['Potato', 'tomato']);
+    assert.deepEqual(without(['ready','freddy'],['let\'s', 'go']),['ready', 'freddy']);
   });
 });
